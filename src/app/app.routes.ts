5b39473dd6d339619ec84b63f81e919259c04d34
Thanh Nguyen <thanh.nguyen@teamrg.com>
2024-07-03 11:47:08 -0400

    add ng materials & schematics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AddressFormComponent } from './schematics/address-form/address-form.component';
+import { TreeComponent } from './schematics/tree/tree.component';
+import { TableComponent } from './schematics/table/table.component';
+import { NavComponent } from './schematics/nav/nav.component';
+import { DragDropComponent } from './schematics/drag-drop/drag-drop.component';
+import { DashboardComponent } from './schematics/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	// { path: '', component: AddressFormComponent },
+	// { path: '', component: DashboardComponent },
+	// { path: '', component: DragDropComponent },
+	// { path: '', component: NavComponent },
+	// { path: '', component: TableComponent },
+	// { path: '', component: TreeComponent },
+];
